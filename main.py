067f251df0b437a22a4705296de76b2d398ca9e7
L4Walk <L4Walk@chuheng.tech>
2023-09-25 21:50:12 +0800

    增加前端页面

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
+from typing import List
 from pydantic import BaseModel
 
 from langchain.document_loaders import WebBaseLoader
@@ -14,10 +16,27 @@ from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import DirectoryLoader
 from langchain.prompts import PromptTemplate
 import os
+import re
+import json
 
 app = FastAPI()
 
-OPENAI_API_KEY = "sk-apikey"
+# 允许所有来源
+origins = [
+    "null",
+    "https://www.bluelskj.com",
+    "http://aitools.chuheng.tech"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+OPENAI_API_KEY = "sk"
 openai_api_key = OPENAI_API_KEY
 
 print("OpenAI API Key:", openai_api_key)
@@ -35,55 +54,98 @@ Helpful Answer:
 
 QA_CHAIN_PROMPT = PromptTemplate.from_template(prompt_template)
 
-#加载数据
+# 加载数据
 """ 嵌入markdown
 loader = UnstructuredMarkdownLoader(
     "./data/AI大学堂.md", mode="elements", strategy="fast",
 )
 """
-loader = DirectoryLoader('./data/', glob="**/*.md", show_progress=True, use_multithreading=True)
+loader = DirectoryLoader('/home/code/blueshirt/AI_Search/data/',
+                         glob="**/*.md", show_progress=True, use_multithreading=False)
 docs = loader.load()
 
-print(f"文档个数：%\n", len(docs))
+print(f"文档个数：\n", len(docs))
 
 
-#文本分割
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
+# 文本分割
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(docs)
 
 # 向量数据库存储
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY))
+vectorstore = Chroma.from_documents(
+    documents=all_splits, embedding=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY))
 
 
-
-#生成
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=OPENAI_API_KEY)
+# 生成
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0,
+                 openai_api_key=OPENAI_API_KEY)
 qa_chain = RetrievalQA.from_chain_type(llm,
                                        retriever=vectorstore.as_retriever(),
                                        return_source_documents=True,
-                                       chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
-)
+                                       chain_type_kwargs={
+                                           "prompt": QA_CHAIN_PROMPT}
+                                       )
 
-#定义请求和响应模型
+
+# 获取title
+def getTitle(doc_metadata_source):
+    match = re.search(r"/data/(.*).md", doc_metadata_source)
+    wp_title = match.group(1) if match else None
+
+    return wp_title
+
+
+json_path = "title2url.json"
+json_data = None
+with open(json_path, 'r', encoding="utf-8") as json_file:
+    json_data = json.load(json_file)
+
+
+def getURL(post_title):
+    return json_data.get(post_title, None)
+
+
+# 定义帖子类型
+class PostData(BaseModel):
+    post_title: str
+    post_url: str
+
+
+# 定义请求和响应模型
 class QueryModel(BaseModel):
     query: str
 
 
 class ResponseModel(BaseModel):
     result: str
-    source_documents: list
+    post_data: List[PostData]
+
 
 @app.post("/ask", response_model=ResponseModel)
 async def ask_question(query: QueryModel):
     result = qa_chain(query.dict())
+
+    post_data_list = []
+    for post in result["source_documents"]:
+        print(post)
+        print(post.metadata)
+        print(post.page_content)
+        post_data_list.append({
+            "post_title": getTitle(post.metadata["source"]),
+            "post_url": getURL(getTitle(post.metadata["source"]))
+        })
+        print(post_data_list)
+
     return {
         "result": result["result"],
-        "source_documents": result["source_documents"]
+        "post_data": post_data_list
     }
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 # 使用相似性搜索检索任何问题的相关拆分。
 """
@@ -97,5 +159,3 @@ result = qa_chain({"query": question})
 print(result["result"])
 print(result["source_documents"])
 """
-
-
